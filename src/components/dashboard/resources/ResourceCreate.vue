1b32ad7aed525c7748875fc7c01bd7e9b7a93e98
Matias Verdier <verdier.matias@gmail.com>
2017-05-28 16:12:20 -0300

    change type to upperCase

--- src/components/dashboard/resources/ResourceCreate.vue
+++ src/components/dashboard/resources/ResourceCreate.vue
@@ -5,83 +5,83 @@
         <h1 class="view-title">{{ viewTitle }}</h1>
       </el-col>
     </el-row>
-    
+
     <el-row type="flex" justify="center">
       <el-col :span="18">
-        
-        <div class="content" v-if="type === 'link'">
+
+        <div class="content" v-if="type === 'LINK'">
           <el-card>
             <el-form :model="resource" label-position="top">
               <el-form-item label="Titulo">
                 <el-input v-model="resource.title" placeholder="De que se trata"></el-input>
               </el-form-item>
-              
+
               <el-form-item label="Url">
                 <el-input v-model="resource.link" placeholder="Inserta tu link"></el-input>
               </el-form-item>
             </el-form>
-            
+
             <el-button @click="backToList">
               Cancelar
             </el-button>
-            
+
             <el-button type="primary" @click="addResource">
               Guardar
             </el-button>
           </el-card>
         </div>
-        
-        
-        <div class="content" v-if="type === 'markdown'">
+
+
+        <div class="content" v-if="type === 'MARKDOWN'">
           <el-card>
             <el-form :model="resource" label-position="top">
               <el-form-item label="Titulo">
                 <el-input v-model="resource.title" placeholder="De que se trata"></el-input>
               </el-form-item>
             </el-form>
-            
+
             <markdown-editor :value="resource.markdown" @input="onInput" :isEditing="true"></markdown-editor>
-            
+
             <el-button @click="backToList">
               Cancelar
             </el-button>
-            
+
             <el-button type="primary" @click="addResource">
               Guardar
             </el-button>
           </el-card>
         </div>
-        
-        <div class="content" v-if="type === 'code'">
+
+        <div class="content" v-if="type === 'CODE'">
           <el-card>
             <el-form :model="resource" label-position="top">
               <el-form-item label="Titulo">
                 <el-input v-model="resource.title" placeholder="De que se trata"></el-input>
               </el-form-item>
             </el-form>
-            
+
             <code-editor></code-editor>
-            
+
             <el-button @click="backToList">
               Cancelar
             </el-button>
-            
+
             <el-button type="primary" @click="addResource">
               Guardar
             </el-button>
           </el-card>
         </div>
-        
+
       </el-col>
     </el-row>
-    
+
     <el-row>
       <el-col>
         <div class="change-type">
           <h3>O intenta con</h3>
-          <router-link to="link" v-show="type !== 'link'">Link</router-link>
-          <router-link to="markdown" v-show="type !== 'markdown'">Markdown</router-link>
-          <router-link to="code" v-show="type !== 'code'">Snippet</router-link>
+          <router-link to="link" v-show="type !== 'LINK'">Link</router-link>
+          <router-link to="markdown" v-show="type !== 'MARKDOWN'">Markdown</router-link>
+          <router-link to="code" v-show="type !== 'CODE'">Snippet</router-link>
         </div>
       </el-col>
     </el-row>
@@ -103,18 +103,18 @@ export default {
   },
   computed: {
     type() {
-      return this.$route.params.type;
+      return this.$route.params.type.toUpperCase();
     },
     viewTitle() {
       let title;
-      if (this.type === 'link') {
+      if (this.type === 'LINK') {
         title = 'Agregar nuevo sitio web';
-      } else if (this.type === 'markdown') {
+      } else if (this.type === 'MARKDOWN') {
         title = 'Crear documento markdown';
-      } else if (this.type === 'code') {
+      } else if (this.type === 'CODE') {
         title = 'Crear snippet';
       }
-      
+
       return title;
     },
   },
@@ -128,11 +128,11 @@ export default {
         title: this.resource.title,
         type,
       };
-      
-      if (type === 'link') data.link = this.resource.link;
-      if (type === 'markdown') data.markdown = this.resource.markdown;
-      if (type === 'code') data.code = this.resource.code;
-      
+
+      if (type === 'LINK') data.link = this.resource.link;
+      if (type === 'MARKDOWN') data.markdown = this.resource.markdown;
+      if (type === 'CODE') data.code = this.resource.code;
+
       this.$store.dispatch('addResource', data)
       .then(() => {
         this.$notify.success({
