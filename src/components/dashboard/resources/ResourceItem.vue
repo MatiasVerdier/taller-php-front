21cd2d4d526ccecc88ff74c0e0232ba11f83fd3b
Matias Verdier <verdier.matias@gmail.com>
2017-06-15 01:18:51 -0300

    indentation

--- src/components/dashboard/resources/ResourceItem.vue
+++ src/components/dashboard/resources/ResourceItem.vue
@@ -3,7 +3,7 @@
     <h2 slot="header" class="title">
       {{ resource.title }}
     </h2>
-    
+
     <div class="resource-content">
       <template v-if="resource.type === 'LINK'">
         <div class="link-image" v-bind:style="{ backgroundImage: `url(${linkImage})` }"></div>
@@ -11,16 +11,16 @@
           <a :href="resource.link" @click.stop="" target="_blank" rel="noopener">Visitar sitio</a>
         </div>
       </template>
-      
+
       <template v-else-if="resource.type === 'MARKDOWN'">
         <markdown-editor :value="resource.markdown" :isEditing="false" :isSmall="true"></markdown-editor>
       </template>
-      
+
       <template v-else="resource.type === 'CODE'">
         {{ resource.code }}
       </template>
     </div>
-    
+
   </el-card>
 </template>
 
@@ -58,27 +58,27 @@ export default {
   flex: 1 0 auto;
   position: relative;
   box-shadow: none;
-  
+
   .el-card__body {
     position: relative;
   }
-  
+
   .el-card__header {
     padding: 10px;
     color: #ecf0f1;
     border: none;
   }
-  
+
   &:hover {
     box-shadow: 0 2px 4px 0 rgba(0,0,0,.12), 0 0 6px 0 rgba(0,0,0,.04);
     cursor: pointer;
   }
-  
+
   .resource-content {
     word-break: break-all;
     overflow: hidden;
     max-height: 180px;
-    
+
     .link-image {
       min-height: 150px;
       background-position: center;
@@ -86,14 +86,14 @@ export default {
       margin-bottom: 10px;
     }
   }
-  
+
   .title {
     font-size: 16px;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
   }
-  
+
   &.type-link {
     .el-card__header {
       background-color: #2980b9;
