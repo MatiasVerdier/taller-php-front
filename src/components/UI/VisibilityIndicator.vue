f63c6d54f6c932b35757c6d3537dcef34de2409e
Matias Verdier <verdier.matias@gmail.com>
2017-07-05 22:29:05 -0300

    change icon for private in visibility indicator

--- src/components/UI/VisibilityIndicator.vue
+++ src/components/UI/VisibilityIndicator.vue
@@ -2,7 +2,7 @@
   <span class="visibility-indicator">
     <i class="fa fa-globe" aria-hidden="true" v-if="status === 'PUBLIC'"></i>
     <i class="fa fa-users" aria-hidden="true" v-if="status === 'SHARED'"></i>
-    <i class="fa fa-shield" aria-hidden="true" v-if="status === 'PRIVATE'"></i>
+    <i class="fa fa-lock" aria-hidden="true" v-if="status === 'PRIVATE'"></i>
   </span>
 </template>
 
