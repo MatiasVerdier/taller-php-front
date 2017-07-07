9c081828bd44da63a992c1c56fad4fa88d0c990d
Matias Verdier <verdier.matias@gmail.com>
2017-07-06 21:26:44 -0300

    resource list with empty content slot and loading indicator

--- src/components/UI/ResourceList.vue
+++ src/components/UI/ResourceList.vue
@@ -1,12 +1,27 @@
 <template lang="html">
-  <el-row class="flex-container">
-    <el-col :xs="24" :sm="12" :lg="6" v-for="resource in resources" class="flex-item" :key="resource.id">
-      <resource-item :resource="resource"></resource-item>
-    </el-col>
-  </el-row>
+  <div class="resource__list">
+    <el-row v-if="!isLoading && !resources.length">
+      <el-col :span="24">
+        <slot name="empty-content">
+          <div class="empty-container">
+            <h2 class="empty-content-title">No hay recursos para mostrar</h2>
+          </div>
+        </slot>
+      </el-col>
+    </el-row>
+
+    <el-row class="flex-container" v-if="!isLoading">
+      <el-col :xs="24" :sm="12" :lg="6" v-for="resource in resources" class="flex-item" :key="resource.id">
+        <resource-item :resource="resource"></resource-item>
+      </el-col>
+    </el-row>
+
+    <div v-show="isLoading" class="loading" v-loading="isLoading" element-loading-text="Cargando"></div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import ResourceItem from './ResourceItem';
 
 export default {
@@ -17,6 +32,9 @@ export default {
       default: [],
     },
   },
+  computed: {
+    ...mapGetters(['isLoading']),
+  },
   components: {
     ResourceItem,
   },
@@ -24,6 +42,14 @@ export default {
 </script>
 
 <style lang="scss">
+  .empty-container {
+    text-align: center;
+  }
+  .empty-content-title {
+    font-size: 3em;
+    padding: 10px 0;
+  }
+
   .flex-container {
     display: flex;
     flex-wrap: wrap;
