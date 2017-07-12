3672d27112cf54bd0d3a79a6974a667dacbcf302
Matias Verdier <verdier.matias@gmail.com>
2017-07-12 17:23:30 -0300

    users list empty message

--- src/components/UI/UserList.vue
+++ src/components/UI/UserList.vue
@@ -1,22 +1,27 @@
 <template lang="html">
-  <el-row class="flex-container">
-    <el-col :xs="24" :sm="12" :md="8" v-for="user in users" class="flex-item" :key="user.id">
-      <el-card>
-        <el-row>
-          <el-col :span="18">
-            <gravatar :email="user.email"></gravatar>
-            <span>{{ user.username }}</span>
-          </el-col>
+  <div>
+    <el-row v-if="users.length === 0">
+      <el-col :span="24">
+        <p>No hay usuarios</p>
+      </el-col>
+    </el-row>
+    <el-row class="flex-container">
+      <el-col :xs="24" :sm="12" :md="8" v-for="user in users" class="flex-item" :key="user.id">
+        <el-card>
+          <el-row>
+            <el-col :span="18">
+              <gravatar :email="user.email"></gravatar>
+              <span>{{ user.username }}</span>
+            </el-col>
 
-          <el-col :span="6">
-            <el-button :plain="true" type="success" style="float: right;">
-              Seguir
-            </el-button>
-          </el-col>
-        </el-row>
-      </el-card>
-    </el-col>
-  </el-row>
+            <el-col :span="6">
+              <follow-status-button :user="user"></follow-status-button>
+            </el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
